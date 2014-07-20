0f5011e2f315ebd80d72945a86212e9130efb5b6
wandonye <dongning.wang@gmail.com>
2014-07-20 22:29:04 +0900

    data displayer
    should display data from weibo

--- wordcrawler.py
+++ wordcrawler.py
@@ -7,17 +7,19 @@ app = Flask(__name__)
 
 
 #webbrowser.open_new(url)
-# 获取URL参数code:
-#code = '2fc0b2f5d2985db832fa01fee6bd9316'
-#client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
-#r = client.request_access_token(code)
-#access_token = r.access_token # 新浪返回的token，类似abc123xyz456
-#expires_in = r.expires_in # token过期的UNIX时间：http://zh.wikipedia.org/wiki/UNIX%E6%97%B6%E9%97%B4
-# TODO: 在此可保存access token
-#print access_token, expires_in
-#client.set_access_token(access_token, expires_in)
+@app.route('/?code=<code>')
+def show_data(code):
+    # show the user profile for that user
+#	code = '4963afb83e6fa5fad0d40e73753babf0'
+	r = client.request_access_token(code)
+	expires_in = r.expires_in
+#	print access_token, expires_in
+	client.set_access_token(access_token, expires_in)
+	content= client.friendships.friends.bilateral.ids.get(uid = 12345678)
 
-#print client.friendships.friends.bilateral.ids.get(uid = 12345678)
+	return render_template('content.html',token=access_token,exp=expires_in,content=content)
+
+#	return 'User %s' % username
 
 @app.route('/')
 def home():
@@ -30,12 +32,6 @@ def home():
 	url = client.get_authorize_url()
 	# TODO: redirect to url
 	return render_template('home.html',url=url)
-	code = '4963afb83e6fa5fad0d40e73753babf0'
-	r = client.request_access_token(code)
-	expires_in = r.expires_in
-	print access_token, expires_in
-	client.set_access_token(access_token, expires_in)
-	print client.friendships.friends.bilateral.ids.get(uid = 12345678)
     
 if __name__ == '__main__':
     app.run()
